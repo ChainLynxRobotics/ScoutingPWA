cff4e3bb80b9aaf3122acf46a443d564775c33f0
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2024-03-02 09:43:14 -0800

    Remove match analytics page data

--- src/pages/analytics/AnalyticsMatchPage.tsx
+++ src/pages/analytics/AnalyticsMatchPage.tsx
@@ -7,37 +7,37 @@ export default function AnalyticsMatchPage() {
 
     const { matchId } = useParams();
 
-    const [hasLoaded, setHasLoaded] = useState(false);
-    const [matches, setMatches] = useState<MatchData[]>([]);
-    const [events, setEvents] = useState<MatchEventData[]>([]);
+    // const [hasLoaded, setHasLoaded] = useState(false);
+    // const [matches, setMatches] = useState<MatchData[]>([]);
+    // const [events, setEvents] = useState<MatchEventData[]>([]);
 
-    useEffect(() => {
-        if (hasLoaded) return;
-        // Load matches for team
-        async function loadMatches() {
-            if (!matchId) return;
-            const matches = await MatchDatabase.getMatchesByMatchId(matchId);
-            matches.sort((a, b) => a.matchId.localeCompare(b.matchId));
-            const events = await MatchDatabase.getEventsByMatch(matchId);
-            setMatches(matches);
-            setEvents(events);
-            setHasLoaded(true);
-        }
-        loadMatches();
-    }, [matchId]);
+    // useEffect(() => {
+    //     if (hasLoaded) return;
+    //     // Load matches for team
+    //     async function loadMatches() {
+    //         if (!matchId) return;
+    //         const matches = await MatchDatabase.getMatchesByMatchId(matchId);
+    //         matches.sort((a, b) => a.matchId.localeCompare(b.matchId));
+    //         const events = await MatchDatabase.getEventsByMatch(matchId);
+    //         setMatches(matches);
+    //         setEvents(events);
+    //         setHasLoaded(true);
+    //     }
+    //     loadMatches();
+    // }, [matchId]);
     
     return (
         <>
             <h1 className="text-xl text-center mb-4">Analytics for <b>match {matchId}</b></h1>
             <span>Not done :(</span>
-            <div className="w-full max-w-md px-2 flex flex-col items-start">
+            {/* <div className="w-full max-w-md px-2 flex flex-col items-start">
                 <h3 className="text-lg mt-4">Known teams in this match: </h3>
                 <ul className="list-disc pl-8">
                     {matches.map(m => m.teamNumber).map((team) => 
                         <li><Link to={'/analytics/team/'+team} key={team} className="text-blue-500 underline">{team}</Link></li>
                     )}
                 </ul>
-            </div>
+            </div> */}
         </>
     )
 }
