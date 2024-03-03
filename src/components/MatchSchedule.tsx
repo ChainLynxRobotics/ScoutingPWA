b8135f05adeb7d2045be13416e80c67b00a8bf7f
Christopher Huntwork <christopher@huntwork.net>
2024-03-03 09:28:58 -0800

    you can now click to switch current match

--- src/components/MatchSchedule.tsx
+++ src/components/MatchSchedule.tsx
@@ -136,7 +136,7 @@ const MatchSchedule = () => {
                 <tbody>
                     {settings.matches.map((match, i)=>(
                         <tr key={match.matchId} className={`mb-1 ${i % 2 == 1 ? 'bg-white bg-opacity-5' : ''} ${settings.currentMatchIndex == i ? 'border-2 border-yellow-300' : ''}`}>
-                            <td scope="row" className="px-2">{match.matchId}</td>
+                            <td scope="row" className="px-2" onClick={() => {settings.setCurrentMatchIndex(i);}}>{match.matchId}</td>
                             <td className="w-full">
                                 <div className="grid grid-cols-3">
                                     <span className="bg-blue-500 bg-opacity-25">{match.blue1}</span>
