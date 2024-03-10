6b3efe652c529dee724eb839fcd32cde2626078f
Tyler Place <79611123+1withspaghetti@users.noreply.github.com>
2024-03-09 23:09:14 -0800

    Screen wake lock

--- src/pages/scout/ScoutLayout.tsx
+++ src/pages/scout/ScoutLayout.tsx
@@ -17,6 +17,26 @@ const ScoutPage = () => {
     const context = useContext(ScoutingContext);
 
     useEffect(() => {
+        async function wakeLock() {
+            if ('wakeLock' in navigator) {
+                try {
+                    const wakeLock = await navigator.wakeLock.request('screen');
+                    wakeLock.addEventListener('release', () => {
+                        console.log('Screen Wake Lock released:', wakeLock.released);
+                    });
+                    return ()=>wakeLock.release();
+                } catch (err) {
+                    console.error("Error requesting Screen Wake Lock:", err);
+                }
+            } else {
+                console.log('Wake Lock API not supported.');
+            }
+        }
+        wakeLock();
+    }, []);
+        
+
+    useEffect(() => {
         if (currentMatchContext?.hasUpdate) {
             setWarningDismissed(false);
         }
