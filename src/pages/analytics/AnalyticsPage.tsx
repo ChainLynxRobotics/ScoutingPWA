4e47ac804db4f764095dd7999485ce8a4207e06e
Tyler Place <79611123+1withspaghetti@users.noreply.github.com>
2024-04-11 17:47:18 -0700

    Start of updated UI for analytics select page

--- src/pages/analytics/AnalyticsPage.tsx
+++ src/pages/analytics/AnalyticsPage.tsx
@@ -1,7 +1,7 @@
-import { Button, Card, Checkbox, FormControl, FormControlLabel, MenuItem, Select, TextField } from "@mui/material";
-import { useContext, useEffect, useState } from "react";
+import { AppBar, Box, Button, Card, Checkbox, FormControl, FormControlLabel, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, MenuItem, Select, Tab, Tabs, TextField, tableCellClasses } from "@mui/material";
+import { useContext, useEffect, useMemo, useState } from "react";
 import MatchDatabase from "../../util/MatchDatabase";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Divider from "../../components/Divider";
 import matchCompare from "../../util/matchCompare";
 import SettingsContext from "../../components/context/SettingsContext";
@@ -12,19 +12,23 @@ const AnalyticsPage = () => {
     const settings = useContext(SettingsContext);
 
     const [search, setSearch] = useLocalStorageState('', 'analyticsSearch');
+    const [tab, setTab] = useState(0);
 
     const [teamList, setTeamList] = useState<number[]>([]);
     const [matchList, setMatchList] = useState<string[]>([]);
     const [contributors, setContributors] = useState<{[key: string]: number}>({});
 
     const [analyticsMatchIndex, setAnalyticsMatchIndex] = useLocalStorageState(settings?.currentMatchIndex||0, "analyticsMatchIndex");
+    const [currentMatchOnly, setCurrentMatchOnly] = useLocalStorageState(false, "analyticsCurrentMatchOnly");
 
-    const analyticsCompetition = settings?.analyticsCurrentCompetitionOnly ? settings?.competitionId : undefined;
     useEffect(() => {
+        const analyticsCompetition = settings?.analyticsCurrentCompetitionOnly ? settings?.competitionId : undefined;
+
         MatchDatabase.getUniqueTeams(analyticsCompetition).then(setTeamList);
         MatchDatabase.getUniqueMatches(analyticsCompetition).then((matches)=>setMatchList(matches.sort(matchCompare)));
         MatchDatabase.getContributions(analyticsCompetition).then((contributors)=>setContributors(contributors));
-    }, [analyticsCompetition]);
+
+    }, [settings?.analyticsCurrentCompetitionOnly, settings?.competitionId]);
 
     function setCurrentMatch(matchId: string) {
         if (!settings) return;
@@ -32,12 +36,22 @@ const AnalyticsPage = () => {
         if (index!==-1) setAnalyticsMatchIndex(index);
     }
 
-    function teamsInCurrentMatch() {
+    const teamsInCurrentMatch = useMemo(() => {
         if (!settings) return [];
         const match = settings.matches[analyticsMatchIndex] || [];
         const teams = [match.blue1, match.blue2, match.blue3, match.red1, match.red2, match.red3]
         return teams.filter(team=>teamList.includes(team));
-    }
+    }, [analyticsMatchIndex, settings, teamList]);
+
+    const sortedTeamList = useMemo(() => {
+        let list = teamList.filter(team=>team.toString().includes(search));
+        if (currentMatchOnly) {
+            list = list.filter(team=>teamsInCurrentMatch.includes(team));
+        }
+        return list.sort((a, b)=>
+            settings?.starredTeams.includes(a) === settings?.starredTeams.includes(b) ? a-b : settings?.starredTeams.includes(a) ? -1 : 1
+        );
+    }, [teamList, search, currentMatchOnly, settings?.starredTeams, teamsInCurrentMatch]);
 
     return (
     <div className="w-full h-full px-4 flex flex-col items-center relative">
@@ -64,48 +78,39 @@ const AnalyticsPage = () => {
                 />
             }
         </div>
-        <div className="w-full max-w-md flex flex-col">
 
-            <h2 className="text-lg font-bold mt-4">Starred Teams</h2>
-            <TeamList teams={teamList.filter(team=>settings?.starredTeams.includes(team))} search={search} />
-
-            <Divider />
-
-            <h2 className="text-lg font-bold mt-4">
-                <span>Teams in match </span>
-                <FormControl variant="standard">
-                    <Select
-                        labelId="match-select-label"
-                        id="match-select"
-                        value={settings?.matches[analyticsMatchIndex]?.matchId}
-                        onChange={(event) => setCurrentMatch(event.target.value)}
-                        label="Select Match">
-                        {settings?.matches.map((match) => {
-                            return <MenuItem key={match.matchId} value={match.matchId}><b>{match.matchId}</b></MenuItem>;
-                        })}
-                    </Select>
-                </FormControl>
-            </h2>
-            <TeamList teams={teamsInCurrentMatch()} search={search} colored />
-
-            <Divider />
-
-            <h2 className="text-lg font-bold mt-4">All Teams</h2>
-            <TeamList teams={teamList} search={search} />
-
-            <Divider />
-
-            <h2 className="text-lg font-bold mt-4">Matches</h2>
-            <div className="flex flex-col gap-2 my-4 px-4">
-                {matchList.map((match) => (
-                    <Link to={`/analytics/match/${match}`} key={match} style={{display: search && !match.includes(search) ? 'none': ''}}>
-                        <Card variant="outlined" className="flex justify-between items-center">
-                            <div className="text-lg font-bold px-2">{match}</div>
-                            <Button endIcon={<span className="material-symbols-outlined">navigate_next</span>}>View</Button>
-                        </Card>
-                    </Link>
-                ))}
+        <div className="w-full max-w-md mt-4 bg-black bg-opacity-20">
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <AppBar position="static">
+                    <Tabs value={tab} onChange={(e,val)=>setTab(val)} variant="fullWidth" aria-label="basic tabs example">
+                        <Tab label="Teams" id="analytics-tab-0" aria-controls="analytics-tabpanel-1" />
+                        <Tab label="Matches" id="analytics-tab-1" aria-controls="analytics-tabpanel-1" />
+                    </Tabs>
+                </AppBar>
+            </Box>
+            <div role="tabpanel" hidden={tab!==0} id="analytics-tabpanel-0" aria-labelledby="analytics-tab-0">
+                <List>
+                    {sortedTeamList.map((team) => (
+                        <TeamListItem team={team} key={team} />
+                    ))}
+                    {sortedTeamList.length===0 &&
+                        <div className="text-center text-secondary my-4">No teams</div>
+                    }
+                </List>
             </div>
+            <div role="tabpanel" hidden={tab!==1} id="analytics-tabpanel-1" aria-labelledby="analytics-tab-1">
+                <List>
+                    {matchList.map((matchId) => (
+                        <MatchListItem matchId={matchId} key={matchId} />
+                    ))}
+                    {matchList.length===0 &&
+                        <div className="text-center text-secondary my-4">No matches</div>
+                    }
+                </List>
+            </div>
+        </div>
+
+        <div className="w-full max-w-md flex flex-col">
 
             <Divider />
 
@@ -123,47 +128,73 @@ const AnalyticsPage = () => {
     )
 }
 
-const TeamList = ({teams, search, colored}: {teams: number[], search: string, colored?: boolean}) => {
+const TeamListItem = ({team}: {team: number}) => {
 
     const settings = useContext(SettingsContext);
+    const navigate = useNavigate();
 
-    function starTeam(e: React.MouseEvent<HTMLButtonElement, MouseEvent>, team: number) {
+    const labelId = `team-list-label-${team}`;
+
+    function toggleStarred(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
         e.stopPropagation();
         e.preventDefault();
         if (settings) {
-            settings.setStarredTeams([...settings.starredTeams, team]);
-        }
-    }
-
-    function unstarTeam(e: React.MouseEvent<HTMLButtonElement, MouseEvent>, team: number) {
-        e.stopPropagation();
-        e.preventDefault();
-        if (settings) {
-            settings.setStarredTeams(settings.starredTeams.filter(t=>t!==team));
+            if (settings.starredTeams.includes(team)) {
+                settings.setStarredTeams(settings.starredTeams.filter(t=>t!==team));
+            } else {
+                settings.setStarredTeams([...settings.starredTeams, team]);
+            }
         }
     }
 
     return (
-        <div className="flex flex-col gap-2 my-4 px-4">
-            {teams.map((team, i) => (
-                <Link to={`/analytics/team/${team}`} key={team} style={{display: search && !team.toString().includes(search) ? 'none': ''}}>
-                    <Card variant="outlined" className="flex justify-between items-center">
-                        <div className="flex items-center gap-2 px-2">
-                            <b className={"text-xl" + (colored ? (i <= 2 ? ' text-blue-400' : ' text-red-400') : '')}>{team}</b>
-                            {settings?.starredTeams.includes(team) ?
-                                <button onClick={(e)=>unstarTeam(e, team)} className="material-symbols-outlined text-yellow-300" style={{fontSize: "20px"}}>star</button>
-                                :
-                                <button onClick={(e)=>starTeam(e, team)} className="material-symbols-outlined text-secondary opacity-50 hover:text-yellow-300" style={{fontSize: "20px"}}>star</button>
-                            }
-                        </div>
-                        <Button endIcon={<span className="material-symbols-outlined">navigate_next</span>}>View</Button>
-                    </Card>
-                </Link>
-            ))}
-            {teams.length===0 &&
-                <div className="text-center text-secondary">No teams</div>
+        <ListItem
+            secondaryAction={
+                <IconButton edge="end" aria-label="expand">
+                    <span className="material-symbols-outlined">navigate_next</span>
+                </IconButton>
             }
-        </div>
+            disablePadding
+            onClick={()=>navigate(`/analytics/team/${team}`)}
+        >
+            <ListItemButton role={undefined}>
+                <ListItemIcon onClick={toggleStarred}>
+                    <Checkbox
+                        edge="start"
+                        checked={settings?.starredTeams.indexOf(team) !== -1}
+                        tabIndex={-1}
+                        inputProps={{ 'aria-labelledby': labelId }}
+                        disableRipple
+                        icon={<span className="material-symbols-outlined">star_outline</span>}
+                        checkedIcon={<span className="material-symbols-outlined">star</span>}
+                    />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={<b>{team}</b>} />
+            </ListItemButton>
+        </ListItem>
+    )
+}
+
+const MatchListItem = ({matchId}: {matchId: string}) => {
+
+    const navigate = useNavigate();
+
+    const labelId = `match-list-label-${matchId}`;
+
+    return (
+        <ListItem
+            secondaryAction={
+                <IconButton edge="end" aria-label="expand">
+                    <span className="material-symbols-outlined">navigate_next</span>
+                </IconButton>
+            }
+            disablePadding
+            onClick={()=>navigate(`/analytics/match/${matchId}`)}
+        >
+            <ListItemButton role={undefined}>
+                <ListItemText id={labelId} primary={<b>{matchId}</b>} />
+            </ListItemButton>
+        </ListItem>
     )
 }
 
